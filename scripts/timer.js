ecb4f463bee0cbfe1c72e03ae8796edee3c0687f
srivastavavrinda13-maker <srivastavavrinda13@gmail.com>
2026-02-15 05:00:53 +0530

    Update timer.js

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,34 +1,41 @@
-let time = 25 * 60; // default 25 min
-let timerInterval = null;
+let totalTime = 25 * 60;
+let time = totalTime;
+let interval = null;
 
-const timerDisplay = document.getElementById("timer");
+const circle = document.getElementById("progressCircle");
+const timerText = document.getElementById("timerText");
 
-function updateDisplay() {
+const radius = 70;
+const circumference = 2 * Math.PI * radius;
+
+circle.style.strokeDasharray = circumference;
+circle.style.strokeDashoffset = 0;
+
+function updateTimer() {
     let min = Math.floor(time / 60);
     let sec = time % 60;
-    timerDisplay.innerText =
-        `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
+
+    timerText.innerText =
+        `${min.toString().padStart(2,'0')}:${sec.toString().padStart(2,'0')}`;
+
+    let progress = time / totalTime;
+    circle.style.strokeDashoffset =
+        circumference * (1 - progress);
 }
 
 function startTimer() {
-    if (timerInterval) return;
+    if(interval) return;
 
-    timerInterval = setInterval(() => {
-        if (time > 0) {
+    interval = setInterval(() => {
+        if(time > 0){
             time--;
-            updateDisplay();
+            updateTimer();
         } else {
-            clearInterval(timerInterval);
-            timerInterval = null;
-            saveFocusSession();
+            clearInterval(interval);
+            interval = null;
             alert("Session complete!");
         }
     }, 1000);
 }
 
-function pauseTimer() {
-    clearInterval(timerInterval);
-    timerInterval = null;
-}
-
-updateDisplay();
+updateTimer();
